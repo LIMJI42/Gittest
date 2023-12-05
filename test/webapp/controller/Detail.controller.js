42a2aac7280417eaa94ccae14e69e196fb24b0e3
LIMJI42 <yoon20614@naver.com>
2023-12-05 12:05:09 +0900

    home & cart 적용

--- test/webapp/controller/Detail.controller.js
+++ test/webapp/controller/Detail.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter"
+    "sap/ui/model/Filter",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,Filter) {
+    function (Controller,JSONModel,Filter,MessageBox) {
         "use strict";
 
         return Controller.extend("test.controller.Detail", {
@@ -15,87 +16,130 @@ sap.ui.define([
 
                 //Detail 라우터에 패턴 매치드 이벤트 붙이기 
                 oRouter.getRoute('RouteDetail').attachPatternMatched(this._patternMatched, this);
-                
                 // var oData = sap.ui.getCore().getModel("List").oData;
-                var oList = this.getView().byId("iDList");
-                
-                // oList.getBinding("ProductCollection",oData);
-
-
+               
+                var oDeliv = new JSONModel({info : [], cart: [], price: ''});
+                this.getView().setModel(oDeliv,'deliv')
                 ////////////
-
+                // var oModel = new JSONModel();
+                // oModel.loadData(_rootPath + "/model/products.json");d
+                // this.getView().setModel(oModel,'main');
+                var oModel = new JSONModel({});
+                // var url = jQuery.sap.getModulePath("test", "/model/products.json")
+                var url = _rootPath+"/model/products.json";
+                    // oModel.attachRequestCompleted(null,function() { 
+                    // console.log(oModel.getData()); 
+                    // this.getView().setModel(oModel) }, 
+                    // this);
+                    oModel.loadData(url, "", false);
+                    this.getView().setModel(oModel, 'main');
+                 
                 this._wizard = this.byId("ShoppingCartWizard");
                 this._oNavContainer = this.byId("navContainer");
                 this._oDynamicPage = this.getPage();
+                // this.model = new JSONModel();
+                // this.model.attachRequestCompleted(null, function () {
+                //     // this.model.getData().ProductCollection.splice(5, this.model.getData().ProductCollection.length);
+                //     this.model.setProperty("/selectedPayment", "Credit Card");
+                //     this.model.setProperty("/selectedDeliveryMethod", "Standard Delivery");
+                //     this.model.setProperty("/differentDeliveryAddress", false);
+                //     this.model.setProperty("/CashOnDelivery", {});
+                //     this.model.setProperty("/BillingAddress", {});
+                //     this.model.setProperty("/CreditCard", {});
+                //     this.model.updateBindings();
+                // }.bind(this));
 
-                this.model = new JSONModel();
-                this.model.attachRequestCompleted(null, function () {
-                    this.model.getData().ProductCollection.splice(5, this.model.getData().ProductCollection.length);
-                    this.model.setProperty("/selectedPayment", "Credit Card");
-                    this.model.setProperty("/selectedDeliveryMethod", "Standard Delivery");
-                    this.model.setProperty("/differentDeliveryAddress", false);
-                    this.model.setProperty("/CashOnDelivery", {});
-                    this.model.setProperty("/BillingAddress", {});
-                    this.model.setProperty("/CreditCard", {});
-                    this.calcTotal();
-                    this.model.updateBindings();
-                }.bind(this));
-
-                this.model.loadData(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
-                this.getView().setModel(this.model);
+                // this.model.loadData(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+                // this.getView().setModel(this.model);
             },
             // 라우터 패턴이 "일치할 때마다" 실행  
             _patternMatched: function(oEvent) {
-                var oList = this.getView().byId("iDList");
-                var oData = sap.ui.getCore().getModel("List")
-                var oView = this.getView();
-                oList.bindAggregation("items",oData);
-                oView.setModel(oData,"List2");
-// <<<<<<< HEAD
-//                 var oDataModel = this.getView().getModel();
-//                 debugger;
-//                 var oFilter = new Filter('Cust_Id','EQ','C00000001'); //Cust_ID 변경!
-//                 oDataModel.read("/CustomerSet",{
-    
-//                     filters: [oFilter] , //sap.ui.model.Filter[] 배열에 필터 객체 여러개 넣을 수 있음
-//                     success: function(oReturn){
-//                         var oCust = oReturn.results[0]
-//                         this.getView().getModel('main').setData(oCust);
-//                     }.bind(this)
-//                 })
-// =======
+                var oModel = this.getView().getModel('main');
+                var oDataModel = this.getView().getModel();
+
+                // var oArgu = oEvent.getParameters().arguments;
+                // var CustId = oModel.getData('/').User[0].CustID;
+                var CustId = oEvent.getParameters().arguments.CustID;
+
+                console.log(CustId);
+                var url = "";
+
+                if(CustId)
+                {
+                    url = "CustomerSet('" + CustId + "')";
+                }
+                else{
+                    this.onMoveApp();
+                }
+                
+                this.calcTotal();
+
+                if(!CustId){
+                    this.getView().getModel().read(url,{
+                        success: function(oReturn){
+                            this.getView().getModel('deliv').setProperty('/info',oReturn);
+                            this.getView().byId('idName').setValue(oReturn.CustName);
+                            this.getView().byId('idPhone').setValue(oReturn.CustTel);
+                            this.getView().byId('idMail').setValue(oReturn.Email);
+                            this.getView().byId('idPost').setValue(oReturn.Postcode);
+                            this.getView().byId('idAddr').setValue(oReturn.CustAddr);
+                            this.getView().byId('idDAddr').setValue(oReturn.DetailAddr);        
+                        }.bind(this)
+                    })    
+                }
+                else{
+                    this.getView().getModel().read(`/CustomerSet('${CustId}')`,{
+                        success: function(oReturn){
+                            this.getView().getModel('deliv').setProperty('/info',oReturn);
+                            this.getView().getModel('deliv').setProperty('/info',oReturn);
+                            this.getView().byId('idName').setValue(oReturn.CustName);
+                            this.getView().byId('idPhone').setValue(oReturn.CustTel);
+                            this.getView().byId('idMail').setValue(oReturn.Email);
+                            this.getView().byId('idPost').setValue(oReturn.Postcode);
+                            this.getView().byId('idAddr').setValue(oReturn.CustAddr);
+                            this.getView().byId('idDAddr').setValue(oReturn.DetailAddr);   
+                        }.bind(this)
+                })}    
             },
             getPage: function () {
                 return this.byId("dynamicPage");
             },
             calcTotal: function () {
-                var data = this.model.getData().ProductCollection;
+                var data = this.getOwnerComponent().getModel('cart').getProperty("/Cart");
+               
                 if (data) {
                     var total = data.reduce(function (prev, current) {
                         prev = prev.Price || prev;
-                        return prev + current.Price;
+                        return Number(prev) + Number(current.Price);
                     });
-                    this.model.setProperty("/ProductsTotalPrice", total.Price || total);
+                    this.getView().getModel('deliv').setProperty('/price', total.Price || total);
                 } else {
-                    this.model.setProperty("/ProductsTotalPrice", 0);
+                    this.getView().getModel('deliv').setProperty('/price', 0);
                 }
             },
     
             handleDelete: function (oEvent) {
-                var listItem = oEvent.getParameter("listItem");
-                var data = this.model.getData().ProductCollection;
+                var listItem = oEvent.getParameter("listItem").getTitle();
+            
+               listItem = listItem.substr(0,2);
+               var data = this.getView().getModel('cart').oData.Cart;
+              
                 if (data.length <= 1) {
                     return;
                 }
     
                 for (var i = 0; i < data.length; i++) {
-                    if (data[i].Name === listItem.getTitle()) {
+                 var dataname = data[i].Name.substr(0,2)
+                    if (dataname === listItem) {
                         data.splice(i, 1);
+                       
+
+                        this.getView().getModel('cart').setProperty('/Cart',data);
+                        debugger;
                         this.calcTotal();
-                        this.model.updateBindings();
+                    
                         break;
-                    }
-                }
+                    } }
             },
     
             goToPaymentStep: function () {
@@ -123,7 +167,7 @@ sap.ui.define([
                     historyPath: "prevPaymentSelect"
                 });
             },
-    
+
             setDifferentDeliveryAddress: function () {
                 this.setDiscardableProperty({
                     message: "Are you sure you want to change the delivery address ? This will discard your progress",
@@ -160,11 +204,11 @@ sap.ui.define([
             },
     
             handleWizardCancel: function () {
-                this._handleMessageBoxOpen("Are you sure you want to cancel your purchase?", "warning");
+                this._handleMessageBoxOpen("구매를 취소하시겠습니까?", "warning");
             },
     
             handleWizardSubmit: function () {
-                this._handleMessageBoxOpen("Are you sure you want to submit your report?", "confirm");
+                this._handleMessageBoxOpen("구매하시겠습니까?", "confirm");
             },
     
             backToWizardContent: function () {
@@ -203,16 +247,83 @@ sap.ui.define([
             },
     
             completedHandler: function () {
+
+                var oBank = this.getView().byId('idBank').getValue();
+                var oCash = this.getView().byId('idCash').getState();
+                var oMoney = this.getView().byId('idMoney').getValue();
+                var oBill = this.getView().byId('idBill').getValue();
+                var oName = this.getView().byId('idName').getValue();
+                var oPhone = this.getView().byId('idPhone').getValue();
+                var oMail = this.getView().byId('idMail').getValue();
+                var oPost = this.getView().byId('idPost').getValue();
+                var oAddr = this.getView().byId('idAddr').getValue();
+                var oDAddr = this.getView().byId('idDAddr').getValue();
+     
+               
+                this.getView().byId('idBank2').setText(oBank);
+                this.getView().byId('idCash2').setState(oCash);
+                this.getView().byId('idMoney2').setText(oMoney);
+                this.getView().byId('idBill2').setText(oBill);
+                this.getView().byId('idName2').setText(oName);
+                this.getView().byId('idPhone2').setText(oPhone);
+                this.getView().byId('idMail2').setText(oMail);
+                this.getView().byId('idPost2').setText(oPost);
+                this.getView().byId('idAddr2').setText(oAddr);
+                this.getView().byId('idDAddr2').setText(oDAddr);
+
                 this._oNavContainer.to(this.byId("wizardBranchingReviewPage"));
+                
             },
-    
+            //구매 시: create
             _handleMessageBoxOpen: function (sMessage, sMessageBoxType) {
                 MessageBox[sMessageBoxType](sMessage, {
                     actions: [MessageBox.Action.YES, MessageBox.Action.NO],
                     onClose: function (oAction) {
                         if (oAction === MessageBox.Action.YES) {
+                            var aCart = this.getView().getModel('cart').oData.Cart;
+                            // aCart 변수에 장바구니 배열 데이터가 들어있음 (cart모델은 json, manifest.json에 등록되어 있음)
+                            var oName = this.getView().byId('idName').getValue();
+                            var oPhone = this.getView().byId('idPhone').getValue();
+                            var oMail = this.getView().byId('idMail').getValue();
+                            var oPost = this.getView().byId('idPost').getValue();
+                            var oAddr = this.getView().byId('idAddr').getValue();
+                            var oDAddr = this.getView().byId('idDAddr').getValue();
+                            // var oDeliv = { 
+                            //                 'CustName': oCart,
+                            //                 'CustTel': oPhone,
+                            //                 'Mail' : oMail,
+                            //                 'Postcode' : oPost,
+                            //                 'Addr': oAddr,
+                            //                 'Detail': oDAddr,
+                            //                 'OrderSet': oCart
+                            //             };
+                            
+
+                            // -------------------------------------------------------------
+                            // Header-Item 구조라서, Deep Create를 해야 됨
+                            var oBody = {
+                                // Header
+                                // 주문 오더에 필요한 헤더 정보를 적어야 됨. 헤더 DB에 추가해야 함
+                                id : '',
+                                name : '',
+                                // Item (장바구니 데이터)
+                                ItemSet : aCart // aCart는 장바구니 배열 데이터.
+                                                // 배열에 들어간 데이터 1건씩
+                                                // 실제 DB Table에 추가되어야 함
+                            };
+
+                            // 위 구조로 create 요청을 보내면 알아서 deep create 로직을 탐!
+                            this.getView().getModel().create("/헤더셋", oBody, {
+                                success: function(oReturn) {},
+                                error :function() {}
+                            });
+                  
+                            // -------------------------------------------------------------
+                            sap.m.MessageToast.show("구매해주셔서 감사합니다.")
                             this._wizard.discardProgress(this._wizard.getSteps()[0]);
-                            this.handleNavBackToList();
+                          
+                            var oRouter = this.getOwnerComponent().getRouter(); 
+                    oRouter.navTo('RouteMain')
                         }
                     }.bind(this)
                 });
@@ -250,7 +361,32 @@ sap.ui.define([
     
                 this._oNavContainer.attachAfterNavigate(fnAfterNavigate);
                 this._oNavContainer.to(this._oDynamicPage);
-// >>>>>>> 3542ea2d1d1a8dd0b5f33c8b2b409f63a6d51cc8
+            },
+            onMoveApp: function () {
+                const oCrossAppNavigator = sap.ushell.Container.getService(
+                  "CrossApplicationNavigation"
+                );
+
+                var custId = this.getView().getModel('CustID').getData();
+
+                const hash =
+                  (oCrossAppNavigator &&
+                    oCrossAppNavigator.hrefForExternal({
+                      target: {
+                        semanticObject: "Z03SE_Login",
+                        action: "display",
+                      }
+                    })) ||
+                  "";
+
+                oCrossAppNavigator.toExternal({
+                  target: {
+                    shellHash: hash,
+                  },
+                });
+            },
+            fnImageSet : function(path) {
+                return _rootPath + path;
             }
         });
     });
--- test/webapp/controller/Main.controller.js
+++ test/webapp/controller/Main.controller.js
@@ -1,20 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Component"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller, JSONModel, Component) {
         "use strict";
 
         return Controller.extend("test.controller.Main", {
             onInit: function () {
                 var oModel = new JSONModel();
-                oModel.loadData("../model/products.json");
+                oModel.loadData(_rootPath + "/model/products.json", "", false);
                 this.getView().setModel(oModel,'Main');
                 // this.byId("idImage").setSrc(_rootPath + "/image/Image1.jpg");
-
+       
                 var oModel2 = new JSONModel({price: []});
                 this.getView().setModel(oModel2,'pricelist');
 
@@ -26,65 +27,118 @@ sap.ui.define([
                 oRouter.getRoute('RouteMain').attachPatternMatched(this._patternMatched, this);
     
             }, _patternMatched:function(oEvent){
-                
-              var  oDataModel = this.getView().getModel();
-              var oModel = this.getView().getModel('pricelist')
-                 oDataModel.read("/PriceSet",{
-                  
-                     success: function(oReturn){
-                       oModel.setProperty('/price',oReturn.results)
-                       debugger;
-                     }.bind(this)
-                 })}
-                
-                 ,
-    
+                var oDataModel = this.getView().getModel();
+                var oModel     = this.getView().getModel('pricelist');
+
+                oDataModel.read("/PriceSet",{
+                        success: function(oReturn){
+                            oModel.setProperty('/price',oReturn.results);
+                        
+                        }.bind(this)
+                });
+
+             
+
+                this.setSlideImagePath();
+                   //Get login Data
+                this.createContent();
+            },
+            createContent : function() {
+                var oModel = new JSONModel({CustID:""});
+                let oComponentData = this.getOwnerComponent().getComponentData();
+                if(!oComponentData.startupParameters.CustID) return;
+                oModel.CustID = decodeURIComponent(oComponentData.startupParameters.CustID[0]);
+                // if(oComponentData && oComponentData.startupParameters && oComponentData.startupParameters.CustID){
+                //     let CustIDstr = decodeURIComponent(oComponentData.startupParameters.CustID[0]).CustID;
+                //     console.log(CustIDstr);
+                //     debugger;
+                // }
+            
+                // console.log(oComponentData);
+                // debugger;
+                // let oQueryParameters = oComponentData.startupParameters.oParameters.oQueryParameters;
+                this.getView().getModel('Main').setProperty('/User', oModel.CustID);
+                // this.getView().setModel(oModel,'CustID');
+
+
+            },
+            setSlideImagePath : function() {
+             
+                this.byId('idImageSlide1').setBackgroundImage(_rootPath + '/image/str1.jpg');
+                this.byId('idImageSlide2').setBackgroundImage(_rootPath + '/image/str2.jpg');
+                this.byId('idImageSlide3').setBackgroundImage(_rootPath + '/image/cucum1.jpg');
+                this.byId('idImageSlide4').setBackgroundImage(_rootPath + '/image/cucum22.jpg');
+                this.byId('idImageSlide5').setBackgroundImage(_rootPath + '/image/tomato1.jpg');
+                this.byId('idImageSlide6').setBackgroundImage(_rootPath + '/image/tomato2.JPG');
+
+            },
             onSelect: function(oEvent) {
                 var oWeight = oEvent.getParameters().selectedItem.mProperties.text;
                 
                 var oWmodel = this.getView().getModel('weight');
                 oWmodel.setData(oWeight);
-              
             },
-            onClick: function(code) {
-                
-                var oWmodel = this.getView().getModel('weight').getData('/');
-                if(oWmodel=='3kg'||oWmodel=='5kg'){
+            // onClick: function(code) {
+            //     var oWmodel = this.getView().getModel('weight').getData('/');
+            //     if(oWmodel=='3kg'||oWmodel=='5kg'){
                    
-                var oDataModel = this.getView().getModel('cart').getProperty('/Cart'); //아무값도 없는 경우엔 []
-                var oPrice = this.getView().getModel().oData.Price;
-                var oPricel = this.getView().getModel('pricelist').getData('/');
-                var oPNum = oPricel.price.find(m => m.ProductNum = code).ProductPrice //해당하는 제품코드의 상품 가격 가져옴
-                var oData = {};
-                var count = 0 ;
-                var success = false;
-                var oWeight = this.getView().byId('idCombo').getSelectedKey();
-                
-               var cart = {
-                            'ProductCod':code,
-                            'Price':oPNum
-               }     
-                oDataModel.push(cart);
-               var oModel = this.getView().getModel('cart');
-                oModel.setProperty('cart',oDataModel)
-                // this.getView().getModel().setData(cart);
-                // var oModel = new sap.ui.model.json.JSONModel(oDataModel,"Cart");
-                // sap.ui.getCore().setModel(oModel,"Cart");
-               success=true;
+            //         var oDataModel = this.getView().getModel('cart').getProperty('/Cart'); //아무값도 없는 경우엔 []
+            //         var oPrice = this.getView().getModel().oData.Price;
+            //         var oPricel = this.getView().getModel('pricelist').getData('/');
+            //         var oPNum = oPricel.price.find(m => m.ProductNum = code).ProductPrice //해당하는 제품코드의 상품 가격 가져옴
+            //         var oData = {};
+            //         var count = 0 ;
+            //         var success = false;
+            //         var oWeight = this.getView().byId('idCombo').getSelectedKey();
+                    
+            //         var cart = {
+            //                         'ProductCod':code,
+            //                         'Price':oPNum
+            //                     };
 
-                if(success)
-                {
-                    sap.m.MessageToast.show("장바구니에 상품을 담았습니다.", {
-                        width: "20em"
-                    });
-                }
-              debugger;  
-                // this.onInit();
-            }else{ sap.m.MessageToast.show("무게를 선택해주세요.", {
-    width: "20em"
-});
+            //         oDataModel.push(cart);
+            //         var oModel = this.getView().getModel('cart');
+            //         oModel.setProperty('cart',oDataModel);
+            //         // this.getView().getModel().setData(cart);
+            //         // var oModel = new sap.ui.model.json.JSONModel(oDataModel,"Cart");
+            //         // sap.ui.getCore().setModel(oModel,"Cart");
+            //         success=true;
 
-            }},
+            //         if(success)
+            //         {
+            //             sap.m.MessageToast.show("장바구니에 상품을 담았습니다.", {
+            //                 width: "20em"
+            //             });
+            //         }
+            //         // this.onInit();
+            //     }
+            //     else{ 
+            //         sap.m.MessageToast.show("무게를 선택해주세요.", {width: "20em"});
+            //     }
+            // },
+            
+            onClick: function(code, isShow, key) {
+                var oModel = this.getView().getModel('cart');
+                var oPricel = this.getView().getModel('pricelist').getData();
+                var aCarts = oModel.getProperty('/Cart') || []; //아무값도 없는 경우엔 []
+                var sPrice = oPricel.price.find(m => m.ProductNum = code).ProductPrice;
+                var oItem = this.getView().getModel('Main').getData().Products.find(m => m.Code == code);
+
+                aCarts.push({
+                    Name            : oItem.Name,
+                    ProductCod      : code,
+                    Price           : sPrice,
+                    Weight          : key || "",
+                    ProductPicUrl   : oItem.ProductPicUrl,  
+                });
+
+                console.log("장바구니 : ", aCarts);
+                oModel.setProperty('/Cart',aCarts);
+                sap.m.MessageToast.show("장바구니에 상품을 담았습니다.", {
+                    width: "20em"
+                });
+            },
+
             onFilterSelect: function(oEvent){
                 //해당하는 obj의 order_num 필요
                     var oOrdNM=oEvent.getParameters().listItem.mAggregations.cells[1].mProperties.text;
@@ -95,18 +149,21 @@ sap.ui.define([
             },
             onFilterSelect: function (oEvent) {
                 var skey = oEvent.getParameter("key");
-    
-                if (skey === "Cart") {
+                var oModel = this.getView().getModel('Main');
+                var User = oModel.getProperty('/User') || []; //아무값도 없는 경우엔 []
+                    
+                if (skey == "Cart") {
+                    
                     var oRouter = this.getOwnerComponent().getRouter(); 
-                    oRouter.navTo('RouteDetail')
-                   
-                } else if (skey === "MyPage") {
-                   
+                    oRouter.navTo('RouteDetail',{
+                        CustID : Users // 필수 파라미터     
+                    });
+                    debugger;
+                } else if (skey === "MyPage") { 
                 } 
-    
             },
             formatPrice: function(oValue){
-                debugger;
+          
 
             },
             fnImageSet : function(path) {
